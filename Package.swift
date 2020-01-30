4253f95027f100d637d3722d5b74594a9a8ccbb7
John Crowson <jtcrowson@gmail.com>
2020-01-30 18:36:40 -0500

    delete unused Package.swift comments

--- Package.swift
+++ Package.swift
@@ -9,13 +9,10 @@ let package = Package(
         .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(name: "ReCombine", targets: ["ReCombine"]),
         .library(name: "ReCombineTest", targets: ["ReCombineTest"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "ReCombine", dependencies: []),
         .testTarget(name: "ReCombineTests", dependencies: ["ReCombine"]),
         .target(name: "ReCombineTest", dependencies: ["ReCombine"]),
